04839871083d1093d563e9181ec99568f7540cdb
Garrett Lancy <garrettlancy@Garretts-MacBook-Air.local>
2019-04-27 18:04:44 -0400

    First Commit

---
+++ src/main/java/brew/brewbrowser/BrewbrowserApplication.java
@@ -0,0 +1,13 @@
+package brew.brewbrowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewbrowserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrewbrowserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/brew/brewbrowser/BrewbrowserApplicationTests.java
@@ -0,0 +1,16 @@
+package brew.brewbrowser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BrewbrowserApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
